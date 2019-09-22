4529b3af634c2e3e61aac083d1b75de8f2c59517
kumikokashii <kkashii@uyahoo.com>
2019-09-22 11:27:38 -0700

    Misc

--- php_rightway/orange.php
+++ php_rightway/orange.php
@@ -2,10 +2,9 @@
 
 header('Content-type: text/plain');
 
-// php is loose on error handling, and tries to keep on processing unless errors are hatal
-
-// @ ignores errors for the following expression
-// Error won't be shown, but saved
+// php is loose on error handling, and tries to keep on processing unless errors are fatal
+// @ ignores warnings for the following expression
+// They won't be shown, but saved
 
 echo "----------------------------\n";
 echo "This is without @\n";
@@ -14,5 +13,25 @@ echo "----------------------------\n";
 echo "This is with @, and print error_get_last()\n";
 echo @(2/0) . "\n";
 print_r(error_get_last());
+echo "\n";
+
+
+// Input validation: https://secure.php.net/filter.filters.validate
+$c = '(bogus@example.org)';
+$sanitized_c = filter_var($c, FILTER_SANITIZE_EMAIL);
+
+if (filter_var($sanitized_c, FILTER_VALIDATE_EMAIL)) {
+    echo "This (c) sanitized email address is considered valid.\n";
+    echo "Before: $c\n";
+    echo "After:  $sanitized_c\n";    
+}
+
+
+// include: runs the file. Prob. not an ideal thing to do in most cases.
+echo "----------------------------\n";
+include 'kiwi.php';
+echo "----------------------------\n";
+
+
 
 ?>
