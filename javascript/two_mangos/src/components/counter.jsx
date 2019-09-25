7fd86bf989ce3ea31ad462ca7920d2c114cf9877
kumikokashii <kkashii@uyahoo.com>
2019-09-24 23:21:43 -0700

    Misc

--- javascript/two_mangos/src/components/counter.jsx
+++ javascript/two_mangos/src/components/counter.jsx
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
+  state = {
+    n: 3
+  }
+
   render() {
-    return <h1>U say goodbye, I say hi!</h1>;
+    return (
+      <div>
+        <h1>U say goodbye, I say hi!</h1>
+        <h3>{this.state.n} times ^_^</h3>
+      </div>
+    )
   }
 }
 
