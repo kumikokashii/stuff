9ccb82d2f578b50e80803cced911c4ab35fcc447
kumikokashii <kkashii@uyahoo.com>
2019-09-28 23:16:05 -0700

    Misc

--- javascript/two_mangos/src/components/counter.jsx
+++ javascript/two_mangos/src/components/counter.jsx
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
+  //constructor() {
+  //  super();
+  //  this.handleIncrement = this.handleIncrement.bind(this);
+  //}
+
   state = {
     n: 1,
-    imageUrl: 'https://picsum.photos/500/300?grayscale'
+    imageUrl: 'https://picsum.photos/500/300?grayscale',
+    tags: ['tetris', 'mario', 'zelda']
   }
 
   render() {
@@ -11,7 +17,17 @@ class Counter extends Component {
       <div>
         <h1>U say goodbye, I say hi!</h1>
         <h3 style={{ fontSize: 17 }} className={this.getBadgeClasses()}>{this.state.n} times ^_^</h3><br/>
-        <button className="btn btn-success btn-sml m-2">Go!</button><br/>
+        <button 
+          onClick={this.handleIncrement}
+          className="btn btn-success btn-sml m-2"
+        >
+          Go!
+        </button><br/>
+
+        <ul>
+          {this.state.tags.map(tag => <li key={tag}>{tag}</li>)}
+        </ul>
+
         <img src={this.state.imageUrl}/>
       </div>
     )
@@ -21,6 +37,10 @@ class Counter extends Component {
     let classes = "badge m-2 badge-";
     classes += this.state.n === 0 ? "warning" : "primary";
     return classes;
+  }
+
+  handleIncrement = () => {
+    this.setState({ n: this.state.n + 1 })
   }
 }
 
