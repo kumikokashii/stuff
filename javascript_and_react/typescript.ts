4c1769c5a74296e4e439c109283cdc3390dd87d5
kumikokashii <kkashii@uyahoo.com>
2020-05-15 12:20:01 -0700

    add typescript

---
+++ javascript_and_react/typescript.ts
@@ -0,0 +1,9 @@
+// TypeScript in 5 minutes: https://www.typescriptlang.org/docs/handbook/typescript-in-5-minutes.html
+// Types: https://www.typescriptlang.org/docs/handbook/basic-types.html
+
+let a: number;
+//a = 'go!;  //error TS2322: Type '"BOO"' is not assignable to type 'number'.
+a = 7*11;
+console.log(a);
+
+// Generics: https://www.digitalocean.com/community/tutorials/typescript-generics-in-typescript
