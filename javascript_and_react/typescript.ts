3f5fa97347b8089e076f381d98c4d2a443d6a554
kumikokashii <kkashii@uyahoo.com>
2020-05-15 14:09:21 -0700

    add generics

--- javascript_and_react/typescript.ts
+++ javascript_and_react/typescript.ts
@@ -2,8 +2,26 @@
 // Types: https://www.typescriptlang.org/docs/handbook/basic-types.html
 
 let a: number;
-//a = 'go!;  //error TS2322: Type '"BOO"' is not assignable to type 'number'.
+//a = 'go!';  //error TS2322: Type '"go!"' is not assignable to type 'number'.
 a = 7*11;
 console.log(a);
 
 // Generics: https://www.digitalocean.com/community/tutorials/typescript-generics-in-typescript
+// With generics
+// - don't need to use specific types. Let T denote a type.
+// - you can limit output type based on input type
+// - you can pass these types to deeper levels
+
+function repeat3times<T>(arg: T): T[] {
+    let output = [];
+    for (let i=0; i < 3; i++) {
+        output.push(arg);
+    }
+    return output;
+}
+
+let strArray: string[];
+strArray = repeat3times('la');
+console.log(strArray);
+//strArray = repeat3times(7);  //error TS2322: Type 'number[]' is not assignable to type 'string[]'.
+
