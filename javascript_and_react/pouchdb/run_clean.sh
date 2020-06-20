3cdd81d74e31361f32d11f8d29ac1486c10b4254
kumikokashii <kkashii@uyahoo.com>
2020-06-20 03:19:45 -0700

    add test pouch

---
+++ javascript_and_react/pouchdb/run_clean.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+clear
+rm -fr fruits
+node test_pouch.js
+
---
+++ javascript_and_react/pouchdb/test_pouch.js
@@ -0,0 +1,96 @@
+// https://pouchdb.com/api.html
+
+const PouchDB = require('pouchdb');
+
+const log = console.log;
+const println = () => {console.log()};
+
+const createDeleteDatabase = async () => {
+  // create a database
+  const db = await new PouchDB('lalala');
+  log('db name =', db.name);
+  println();
+
+  // note: can't list all databases
+
+  // delete a database
+  // the db still exists. marks _destroyed = true.
+  log('before destroy, _destroyed =', db._destroyed); // undefined
+
+  await db.destroy()
+    .then((res) => {
+      log('after destroy, _destroyed =', db._destroyed); // true
+    })
+    .catch((err) => {
+      log('error destroying', db.name);
+    })
+    
+  println();
+}
+
+const printDocs = async (db) => {
+  await db.allDocs({include_docs: true,})
+  .then((result) => {
+    result.rows.forEach((row) => {
+      log(row.doc)
+    });
+  })
+  .catch((err) => {
+    log(err);
+  });
+}
+
+const createFetchDeleteDoc = async () => {
+  const db = await new PouchDB('fruits');
+
+  // use PUT. specify ur own id
+  await db.put({_id: '01', name: 'pineapple'})
+    .then(() => {})
+    .catch(() => {});
+  
+  // don't use POST. pouch will set an id
+  await db.post({name: 'strawberry'})
+    .then(() => {})
+    .catch(() => {});
+  
+  await printDocs(db);
+  // {
+  //   name: 'pineapple',
+  //   _id: '01',
+  //   _rev: '1-233588379ca846bb9328744426c54424'
+  // }
+  // {
+  //   name: 'strawberry',
+  //   _id: '4c98675b-41b0-4ca8-8aa0-3a563bb6bd4a',
+  //   _rev: '1-0ed990039341f6af6d1d7afc851a3393'
+  // }
+  
+  println();
+  
+  // simple fetch of a doc by _id
+  log('fetch doc with _id = 01');
+  await db.get('01')
+    .then((doc) => log(doc))
+    .catch((err) => log(err));
+
+  println();
+
+  // to delete, provide the whole doc
+  log('gonna remove doc with _id = 01');
+  await db.get('01')
+    .then(async (doc) => {
+      await db.remove(doc)
+        .then((result) => log(result))
+        .catch((err) => log(err))
+    .catch((err) => log(err))
+    });
+
+  println();
+}
+
+const main = async () => {
+  await createDeleteDatabase();
+  await createFetchDeleteDoc();
+}
+
+main();
